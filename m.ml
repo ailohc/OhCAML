257b2944e52402626c30f336542b2b76f70f06e1
Henry Lee <gbvrcx@gmail.com>
2018-02-24 01:25:22 +0900

    basic structure of functions

--- m.ml
+++ m.ml
@@ -15,11 +15,90 @@ and exp =
   | CALL of exp * exp
 and var = string
 
-exception TypeError
-
 type typ = TyInt | TyBool | TyFun of typ * typ | TyVar of tyvar
 and tyvar = string
-type typ_eqn = (typ * typ) list
+
+type equation_op = Add | Sub | Mul | Div
+and sym_value =
+  (* value *)
+  | Int of int
+  | Bool of bool
+  | Fun of var * exp * sym_env
+  | FunRec of var * var * exp * sym_env
+  (* symbol *)
+  | SInt of id
+  | SBool of id
+  | SFun of id * typ * typ
+  (* equation *)
+  | SEqu of equation_op * sym_value * sym_value
+and id = int
+and sym_env = (var * sym_value) list
+
+let sym_cnt = ref 0
+let init_sym_cnt () = sym_cnt := 0
+let new_sym () = sym_cnt := !sym_cnt + 1; !sym_cnt
+
+let empty_env = []
+let rec find env x =
+  match env with
+  | [] -> raise (Failure "Env is Empty")
+  | (y, v)::tl -> if y = x then v else find tl x
+let append env (x, v) = (x, v)::env
+
+type path_exp =
+  (* boolean exp *)
+  | TRUE
+  | FALSE
+  (* boolean op *)
+  | AND of path_exp * path_exp
+  | OR of path_exp * path_exp
+  | NOT of path_exp
+  (* symbolic equation *)
+  | EQUAL of sym_value * sym_value
+  | NOTEQ of sym_value * sym_value
+and path_cond = path_exp
+
+let default_path_cond = TRUE
+
+exception NotImplemented
+
+let sym_eval : exp -> sym_env -> path_cond -> (sym_value * path_cond)
+= fun e env pi ->
+  match e with
+  | CONST n -> (Int n, pi)
+  | VAR x -> (find env x, pi)
+  | ADD (e1, e2) -> raise NotImplemented
+  | SUB (eq, e2) -> raise NotImplemented
+  | MUL (eq, e2) -> raise NotImplemented
+  | DIV (eq, e2) -> raise NotImplemented
+  | ISZERO e -> raise NotImplemented
+  | READ -> raise NotImplemented
+  | IF (cond, e1, e2) -> raise NotImplemented
+  | LET (x, e1, e2) -> raise NotImplemented
+  | LETREC (f, x, e1, e2) -> raise NotImplemented
+  | PROC (x, e) -> raise NotImplemented
+  | CALL (e1, e2) -> raise NotImplemented
+
+(* environment generator *)
+let rec gen_senv : (var * typ) list -> sym_env -> sym_env
+= fun args r ->
+  match args with
+  | [] -> r  
+  | (x, tp)::tl ->
+    let r = begin
+      match tp with
+      | TyInt -> append r (x, SInt (new_sym ()))
+      | TyBool -> append r (x, SBool (new_sym ()))
+      | TyFun (t1, t2) -> append r (x, SFun (new_sym (), t1, t2))
+      | TyVar t -> raise NotImplemented
+    end in
+    gen_senv tl r
+  
+let equal : exp -> exp -> typ list -> bool
+= fun p1 p2 typs -> raise NotImplemented
+
+(*
+exception TypeError
 
 let rec string_of_type ty = 
   match ty with
@@ -141,3 +220,4 @@ let rec typeof : exp -> typ
       print_endline "";
       ty
   with TypeError -> (print_endline "The program does not have type. Rejected."); exit (1)
+*)
