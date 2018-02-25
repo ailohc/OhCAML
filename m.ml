271fd127ce55f590474a1d92eaf570541bac6561
Henry Lee <gbvrcx@gmail.com>
2018-02-25 13:23:18 +0900

    undo prev commit

--- m.ml
+++ m.ml
@@ -68,11 +68,6 @@ let sym_eval : exp -> sym_env -> path_cond -> (sym_value * path_cond)
   match e with
   | CONST n -> (Int n, pi)
   | VAR x -> (find env x, pi)
-  | SInt sn -> (SInt sn, pi)
-  | SBool sb -> (SBool sb, pi)
-  | SVar sx -> (SVar sx, pi)
-  | SFun (sarg, t1, t2) -> (*todo*)
-  | SExp (saop, s1, s2) -> (*todo*)
   | ADD (e1, e2) -> raise NotImplemented
   | SUB (eq, e2) -> raise NotImplemented
   | MUL (eq, e2) -> raise NotImplemented
@@ -84,7 +79,6 @@ let sym_eval : exp -> sym_env -> path_cond -> (sym_value * path_cond)
   | LETREC (f, x, e1, e2) -> raise NotImplemented
   | PROC (x, e) -> raise NotImplemented
   | CALL (e1, e2) -> raise NotImplemented
-
 
 let rec find_sym_var : sym_env-> var -> sym_env =
   fun senv x ->
