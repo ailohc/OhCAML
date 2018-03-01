8dd2d09f23af493c7bef3a223734ced89a366100
dainlee <ailohc@naver.com>
2018-03-01 18:01:30 +0900

    update iszero

--- m.ml
+++ m.ml
@@ -115,9 +115,11 @@ let rec sym_eval : exp -> sym_env -> path_cond -> (sym_value * path_cond)
     let (v, pi) = sym_eval e env pi in
     begin
       match v with
-      | Int 0 -> (true, AND(pi, EQUAL(v, Int 0)))
-      | Int _ -> (false, AND(pi, NOTEQ(v, Int 0)))
-      | _ -> raise NotImplemented (*TODO*)
+      | Int 0 -> (Bool true, AND(pi, EQUAL(v, Int 0)))
+      | Int _ -> (Bool false, AND(pi, NOTEQ(v, Int 0)))
+      | SInt n -> raise NotImplemented (*TODO*)
+      | SExp (saop, sv1, sv2) -> raise NotImplemented (*TODO*) 
+      | _ -> raise SyntaxError
   | READ -> (SInt (new_sym ()), pi)
   | IF (cond, e1, e2) ->
     let (b, pi) = sym_eval cond env pi in
