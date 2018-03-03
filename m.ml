40cc5660e5dc77375e6eb7ba9ef1b78a6801668a
Henry Lee <gbvrcx@gmail.com>
2018-03-04 01:21:03 +0900

    add simplify_val

--- m.ml
+++ m.ml
@@ -41,6 +41,7 @@ type sym_value =
   | SFun of id * typ * typ
   (* arithmetic expression *)
   | SExp of arithmetic_op * sym_value * sym_value
+  | SMinus of sym_value
   (* end of recursive *)
   | EoR of var
 and arithmetic_op = SADD | SSUB | SMUL | SDIV
@@ -59,6 +60,50 @@ let rec find env x =
   | [] -> raise (Failure "Env is Empty")
   | (y, v)::tl -> if y = x then v else find tl x
 let append env (x, v) = (x, v)::env
+
+let rec fix_point : 'a -> ('a -> 'a) -> 'a
+= fun x f-> let next = f x in if x = next then x else fix_point next f
+
+let rec simplify_val_aux : sym_value -> sym_value
+= fun v ->
+  match v with
+  | Int _ | Bool _ | Fun _ | FunRec _ | SInt _ | SBool _ | SVar _ | SFun _ | EoR _ -> v
+  | SExp (aop, v1, v2) ->
+    let v1, v2 = simplify_val_aux v1, simplify_val_aux v2 in
+    begin
+      match aop with
+      | SADD -> 
+        begin
+          match v1, v2 with
+          | EoR f, _ | _, EoR f -> EoR f
+          | _ -> v (* TODO *)
+        end
+      | SSUB -> SExp (SADD, v1, SMinus v2)
+      | SMUL ->
+        begin
+          match v1, v2 with
+          | EoR f, _ | _, EoR f -> EoR f
+          | _ -> v (* TODO *)
+        end
+      | SDIV ->
+        begin
+          match v1, v2 with
+          | EoR f, _ | _, EoR f -> EoR f
+          | _ -> v (* TODO *)
+        end
+      end
+  | SMinus v ->
+    begin
+      match v with
+      | Int n -> Int (-n)
+      | Bool _ | Fun _ | FunRec _ | SInt _ | SBool _ | SVar _ | SFun _ -> raise (Failure "Not Integer Value") (* Should not reach heer *)
+      | EoR _ -> v
+      | SExp _ -> SMinus (simplify_val_aux v)
+      | SMinus v -> v
+    end
+
+let simplify_val : sym_value -> sym_value
+= fun v -> fix_point v simplify_val_aux
 
 let rec value2str : sym_value -> string
 = fun v ->
@@ -79,6 +124,7 @@ let rec value2str : sym_value -> string
       | SMUL -> "(" ^ value2str v1 ^ " * " ^ value2str v2 ^ ")"
       | SDIV -> "(" ^ value2str v1 ^ " / " ^ value2str v2 ^ ")"
     end
+  | SMinus v -> "(-" ^ value2str v ^ ")"
   | EoR f -> "Can't eval: fun " ^ f ^ " called more than " ^ string_of_int recursive_cnt ^ " times recursively"
 
 type path_exp =
@@ -102,25 +148,24 @@ let rec simplify_cond : path_exp -> path_exp
   | TRUE -> TRUE
   | FALSE -> FALSE
   | AND (e1, e2) ->
-    let v1 = simplify_cond e1 in
-    let v2 = simplify_cond e2 in
+    let e1, e2 = simplify_cond e1, simplify_cond e2 in
     begin
-      match v1, v2 with
-      | TRUE, _ -> v2
+      match e1, e2 with
+      | TRUE, _ -> e2
       | FALSE, _ -> FALSE
-      | _ -> AND (v1, v2)
+      | _ -> AND (e1, e2)
     end
   | OR (e1, e2) ->
-    let v1 = simplify_cond e1 in
-    let v2 = simplify_cond e2 in
+    let e1, e2 = simplify_cond e1, simplify_cond e2 in
     begin
-      match v1, v2 with
+      match e1, e2 with
       | TRUE, _ -> TRUE
-      | FALSE, _ -> v2
-      | _ -> OR (v1, v2)
+      | FALSE, _ -> e2
+      | _ -> OR (e1, e2)
     end
   | NOT e -> NOT (simplify_cond e)
-  | EQUAL _ | NOTEQ _ -> pi
+  | EQUAL (v1, v2) -> EQUAL (simplify_val v1, simplify_val v2)
+  | NOTEQ (v1, v2) -> NOTEQ (simplify_val v1, simplify_val v2)
 
 let rec cond2str : path_exp -> string
 = fun pi ->
