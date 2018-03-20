d96cf5b40be0c68cf5e904042fd40abfe068d72a
dainlee <ailohc@naver.com>
2018-03-20 18:02:21 +0900

    still cannot print solve

--- simplify.ml
+++ simplify.ml
@@ -8,9 +8,12 @@ let simplify_val : sym_value -> sym_value
   let expr = simplify expr None in
  print_endline (to_string expr); expr2val expr
 
+
+
 let simplify_path : path_exp -> path_exp
 = fun p ->
   let expr = path2expr p in
   let expr = simplify expr None in
-  print_endline (to_string expr); get_path_sat expr; expr2path expr
+  let cond_solver = get_path_sat expr in
+  print_endline (to_string expr); (*print_endline (Z3.Solver.to_string cond_solver);*) expr2path expr
 
