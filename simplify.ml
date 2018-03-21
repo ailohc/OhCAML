9e204e004c068c44bccd418989c07165bcb30bb8
Henry Lee <gbvrcx@gmail.com>
2018-03-21 17:17:50 +0900

    Revert "getting assertions"
    This reverts commit db23098b1b350781d44145193aab21df8e943524.

--- simplify.ml
+++ simplify.ml
@@ -1,14 +1,16 @@
 open Lang
-open Solve
 open Z3.Expr
 open Z3_translator
 
-let simplify_val v =
+let simplify_val : sym_value -> sym_value
+= fun v ->
   match v with
   | Error _ | EoR _ -> v
   | _ -> let expr = val2expr v in let expr = simplify expr None in expr2val expr
 
 let simplify_path : path_exp -> path_exp
-= fun p -> let expr = path2expr p in let expr = simplify expr None in let assertions = get_assertion expr in expr2path expr
+= fun p -> let expr = path2expr p in let expr = simplify expr None in expr2path expr
 
+  (*let cond_solver = get_path_sat expr in
+  print_endline (to_string expr); (*print_endline (Z3.Solver.to_string cond_solver);*) expr2path expr*)
 
