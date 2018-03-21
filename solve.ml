eb78e47f64814bb18202024737be29c268133aaf
dainlee <ailohc@naver.com>
2018-03-21 18:25:13 +0900

    brute_force solve

--- solve.ml
+++ solve.ml
@@ -2,6 +2,8 @@ open Lang
 open Z3_translator
 open Z3.Solver
 
+exception CannotCompare
+
 let rec find_sym_var : sym_env-> var -> sym_env =
   fun senv x ->
   match senv with
@@ -34,5 +36,27 @@ let sat_check : path_cond -> bool
   | UNKNOWN -> false
   | SATISFIABLE -> true
 
-let solve : (sym_value * path_cond) list -> (sym_value * path_cond) list -> bool
-= fun t1 t2 -> raise (Failure "solve: Not Implemented") (* TODO *)
+(*let rec compare_list = []; 
+
+let rec list_solve_aux : sym_value -> sym_value -> bool list
+= fun s1 s2 -> if s1 = s2 then true::compare_list else false::compare_list
+
+let rec list_solve : (sym_value * path_cond) list -> (sym_value * path_cond) list -> bool list
+= fun lst1 lst2 -> 
+  match lst1 with
+  | (s1, p1)::t1 -> 
+    match lst2 with
+    | (s2, p2)::t2 -> if p1 = p2 then list_solve_aux s1 s2; list_solve t1 t2 else list_solve lst1 t2
+    | _ -> raise CannotCompare *)
+
+let rec solve_aux : (sym_value * path_cond) -> (sym_value * path_cond) list -> bool
+= fun v1 v2_list ->
+  match v2_list with
+  | [] -> false
+  | hd::tl -> if v1 = hd then true else solve_aux v1 tl
+
+let rec solve : (sym_value * path_cond) list -> (sym_value * path_cond) list -> bool
+= fun v1_list v2_list ->
+  match v1_list with
+  | [] -> true (**)
+  | hd::tl -> (solve_aux hd v2_list) && (solve tl v2_list)
